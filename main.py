62ee5834dcabe32e119339a49083f97463bf243f
khaledbs17 <105543186+khaledbs17@users.noreply.github.com>
2025-02-06 09:38:45 +0100

    change output

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
+
 def main():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+        print(fizzbuzz(i))
 
 if __name__ == "__main__":
     main()
